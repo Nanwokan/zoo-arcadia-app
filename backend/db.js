8c60019c5c8911e81eba1fe5884292b512093b18
Nanwokan <now_okan@outlook.fr>
2025-04-08 15:32:35 +0200

    Fix: correction du next(err) => next(error) dans loginUser.js

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,22 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
-
-dotenv.config()
+dotenv.config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,       // "localhost"
-    user: process.env.DB_USER,       // "root"
-    password: process.env.DB_PASSWORD, // ton mot de passe
-    database: process.env.DB_NAME    // "arcadiadb"
+  host: process.env.DB_HOST,            // switchyard.proxy.rlwy.net
+  port: process.env.DB_PORT,            // 43484
+  user: process.env.DB_USER,            // root
+  password: process.env.DB_PASSWORD,    // le mot de passe railway
+  database: process.env.DB_NAME,        // railway
+  connectTimeout: 10000                 // en ms, pour éviter l’ETIMEDOUT
 });
 
 db.connect((err) => {
-    if (err) {
-      console.error('❌ Erreur de connexion :', err.message);
-      return;
-    }
-    console.log('✅ Connecté à la base MySQL !');
+  if (err) {
+    console.error('❌ Erreur de connexion :', err.message);
+    return;
+  }
+  console.log('✅ Connecté à la base MySQL !');
 });
-  
+
 module.exports = db;
